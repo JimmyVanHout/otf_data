951f66d64656a548c8e8be05762c08669c111f36
Jimmy Van Hout <jimmyvanhout40@gmail.com>
2022-02-24 20:53:16 -0500

    improved user interface

--- server/static/user_interface.js
+++ server/static/user_interface.js
@@ -1,9 +1,9 @@
 function displayOnCheck(checkbox) {
     let div = document.getElementById("forwarding_input");
     if (checkbox.checked) {
-        let ids = ["forwarding_email_address", "forwarding_password", "forwarding_receiving_email_address", "forwarding_mailbox"];
-        let descriptions = ["Enter email address of forwarding account: ", "Enter password of forwarding account: ", "Enter receiving email address: ", "Enter mailbox to search (left blank, default is inbox): "];
-        let input_types = ["text", "password", "text", "text"];
+        let ids = ["forwarding_email_address", "forwarding_password", "forwarding_confirm_password", "forwarding_receiving_email_address", "forwarding_mailbox"];
+        let descriptions = ["Enter email address of forwarding account: ", "Enter password of forwarding account: ", "Confirm password of forwarding account: ", "Enter receiving email address: ", "Enter mailbox to search (left blank, default is inbox): "];
+        let input_types = ["email", "password", "password", "email", "text"];
         h2 = document.createElement("h2");
         h2.id = "forwarding_heading";
         h2.innerText = "One-Time Forwarding of All Past OT Beat Report Emails from This Account:";
@@ -15,6 +15,7 @@ function displayOnCheck(checkbox) {
             input.id = ids[i];
             label = document.createElement("label");
             label.for = input.id;
+            label.id = input.id.concat("_label");
             label.innerText = descriptions[i];
             div.appendChild(label);
             div.appendChild(input);
@@ -29,4 +30,37 @@ function displayOnCheck(checkbox) {
         h2 = document.getElementById("searching_heading");
         h2.innerText = "Get OT Beat Report Data from This Account:";
     }
+}
+
+function checkPasswordsMatch(password_id, confirm_password_id, error_message, error_id) {
+    let password = document.getElementById(password_id);
+    let confirm_password = document.getElementById(confirm_password_id);
+    let div = document.getElementById("above_submit");
+    if (password.value != confirm_password.value) {
+        if (Array.from(div.children).filter(child => child.id == error_id).length == 0) {
+            let p = document.createElement("p");
+            p.className = "error_message";
+            p.innerText = error_message;
+            p.id = error_id;
+            div.appendChild(p);
+        }
+        console.log(password.value);
+        console.log(confirm_password.value);
+        return false;
+    } else {
+        let p = document.getElementById(error_id);
+        div.children[Array.from(div.children).indexOf(p)].remove();
+        console.log(true);
+        return true;
+    }
+}
+
+function checkAllPasswordsMatch() {
+    let forwarding_password = document.getElementById("forwarding_password");
+    let forwarding_passwords_match = true;
+    if (forwarding_password) {
+        forwarding_passwords_match = checkPasswordsMatch("forwarding_password", "forwarding_confirm_password", "Passwords for account to forward messages from do not match.", "forwarding_passwords_error");
+    }
+    let searching_passwords_match = checkPasswordsMatch("password", "confirm_password", "Passwords for account to get OT data from do not match.", "searching_passwords_error");
+    return forwarding_passwords_match && searching_passwords_match;
 }
